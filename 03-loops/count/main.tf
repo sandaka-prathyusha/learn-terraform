45304e982a03843f6639782c05b9d1d5336cf214
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-07-20 19:16:07 +0530

    null

--- 03-loops/count/main.tf
+++ 03-loops/count/main.tf
@@ -2,6 +2,6 @@ resource "null_resource" "null" {}
 
 # if i want to run the above resource 10 times the "count"will come into picture.
 
-resource "null_resource" "null" {
+resource "null_resource" "null1" {
   count = 10
 }
