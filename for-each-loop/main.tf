07f754542420514894d43c8d2586580ecbb48840
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-07-24 12:54:19 +0530

    for each

--- for-each-loop/main.tf
+++ for-each-loop/main.tf
@@ -5,19 +5,20 @@
 # if i want to run the above resource 10 times the "count"will come into picture.
 
 resource "null_resource" "fruits" {
-   count = length(var.fruits)
+
+ for_each = var.fruits
 
   provisioner "local-exec" {
-    command = "echo fruit name - ${var.fruits[count.index]}"
+    command = "echo fruit name - ${var.fruits[each.key]} - ${var.fruits[each.value]}"
 #    command = "echo ${length(var.fruits)}"
   }
-
 }
-variable "fruits" {
-  default = {
-    apple = 400
-    orange = 200
-    banana = 100
+
+ variable "fruits" {
+   default = {
+     apple = 400
+     orange = 200
+     banana = 100
   }
 }
 
