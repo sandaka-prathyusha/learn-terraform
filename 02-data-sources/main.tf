d0fdbdc05138725070040a055a90d453178510dd
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-10-10 14:50:15 +0530

    ec2

--- 02-data-sources/main.tf
+++ 02-data-sources/main.tf
@@ -5,3 +5,9 @@ data "aws_security_group" "selected" {
 output "security_group_id" {
   value = data.aws_security_group.selected.id
 }
+
+data "aws_ami" "example" {
+
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
