115d6fd8e2aed5338ad14f9270eb53a4198ae9cb
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-10-10 15:52:55 +0530

    datasorce

--- 02-data-sources/main.tf
+++ 02-data-sources/main.tf
@@ -1,13 +1,18 @@
 data "aws_security_group" "selected" {
   name = "allow-all"
-}
+  }
 
 output "security_group_id" {
   value = data.aws_security_group.selected.id
-}
+  }
 
 data "aws_ami" "example" {
 
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+  output "aws_ami_id"  {
+   value = data.aws_ami.example.id
+  }
